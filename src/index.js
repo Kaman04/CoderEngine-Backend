d9ab2a22a43ae5ea29656c62068e73b0be4daff4
Aman Kumar <itsamankumar2004@gmail.com>
2025-09-20 23:29:01 +0530

    Fix CORS for Vercel frontend

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const cors = require('cors')
 // console.log("Hello")
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://coder-engine.vercel.app',
     credentials: true 
 }))
 
