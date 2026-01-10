c86184037386d33ccadfaa70618413140df64432
Aman Kumar <itsamankumar2004@gmail.com>
2026-01-10 20:48:23 +0530

    fix redis host and port

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,11 +1,20 @@
 const { createClient }  = require('redis');
 
+// const redisClient = createClient({
+//     username: 'default',
+//     password: process.env.REDIS_PASS,
+//     socket: {
+//         host: 'redis-19564.crce206.ap-south-1-1.ec2.redns.redis-cloud.com',
+//         port: 19564
+//     }
+// });
+
 const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-19564.crce206.ap-south-1-1.ec2.redns.redis-cloud.com',
-        port: 19564
+        host: 'redis-16999.c301.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 16999
     }
 });
 
